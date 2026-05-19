d74b02df584aaa720c277c9b783231d20e58acdb
seojin <seojin@example.com>
2026-05-19 23:23:00 +0900

    feat: add build.bat for easy windows build

---
+++ build.bat
@@ -0,0 +1,25 @@
+@echo off
+echo [Building SRPG for Windows...]
+
+set CC=gcc
+set CFLAGS=-Wall -Wextra -std=c11 -Iinclude -I"C:/raylib/include"
+set LDFLAGS=-L"C:/raylib/lib" -lraylib -lgdi32 -lwinmm
+
+:: Create objects
+%CC% %CFLAGS% -c src/main.c -o src/main.o
+%CC% %CFLAGS% -c src/game.c -o src/game.o
+%CC% %CFLAGS% -c src/map.c -o src/map.o
+%CC% %CFLAGS% -c src/entity.c -o src/entity.o
+%CC% %CFLAGS% -c src/player.c -o src/player.o
+
+:: Link
+%CC% src/main.o src/game.o src/map.o src/entity.o src/player.o -o srpg.exe %LDFLAGS%
+
+if %errorlevel% equ 0 (
+    echo [Build Successful!]
+    echo Running srpg.exe...
+    ./srpg.exe
+) else (
+    echo [Build Failed!]
+    pause
+)
